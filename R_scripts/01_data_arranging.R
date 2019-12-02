12c7d03d6c4e7d6cabfebf7871aa7ed3c5af118a
Philipp Lange <40263931+PhilLange@users.noreply.github.com>
2019-12-02 09:47:57 +0100

    Update 01_data_arranging.R

--- R_scripts/01_data_arranging.R
+++ R_scripts/01_data_arranging.R
@@ -1,60 +1,88 @@
 library(dplyr)
-library(plyr)
 library(ggplot2)
+library(psych)
+library(car)
+
+# === 1) get behavioural data =============================
 setwd("/Users/philipplange/Desktop/analysis_ma/behav/")
 
 #load all reaction time files and convert to dataframe
 files  <- list.files(pattern = '\\.tsv')
 tables <- lapply(files, read.table, header = TRUE, sep = "\t")
-behav_data <- do.call(rbind , tables)
+behav_data <- do.call(dplyr::bind_rows, tables)
  
 # round reaction time  to 3 digits
-behav_data[,'rt'] <- round(behav_data[,1],3)
+behav_data[,'rt'] <- round(behav_data[, 'rt'], 3)
 
-#condition as factor
-behav_data$condition <- as.factor(behav_data$condition)
-#subject as factor
-behav_data$subject <- as.factor(behav_data$subject)
+#condition as int
+behav_data$condition <- as.integer(behav_data$condition)
+#subject as int
+behav_data$subject <- as.integer(behav_data$subject)
+# add epoch column
+behav_data <- behav_data %>% 
+  group_by(subject) %>%
+  mutate(epoch = (1:sum(!is.na(rt))-1))
+
+
+# === 2) get eeg data ==========================================
 setwd("/Users/philipplange/Desktop/analysis_ma/epochs/")
 
 #load all eeg files and convert to dataframe
 files  <- list.files(pattern = '\\.tsv')
-tables <- lapply(files, read.table, header = TRUE, sep = "\t")
-eeg_data <- do.call(dplyr::bind_rows , tables)
+tables <- lapply(files, read.table, 
+                 header = T, 
+                 sep = "\t",
+                 colClasses = c('numeric', 'numeric',
+                                'character', 
+                                'numeric', 'numeric'))
+eeg_data <- do.call(dplyr::bind_rows, tables)
 
 # groupy by subject, condition, epoch to compute mean amplitude
-eeg_data <- eeg_data %>% group_by(condition, subject, epoch) %>% 
+eeg_data <- eeg_data %>% 
   group_by(epoch, condition, subject) %>%
-  summarise(mean_amp_FCz = mean(FCz),
-            mean_amo_Cz  = mean(Cz))
+  dplyr::summarise(mean_amp_FCz = mean(FCz),
+            mean_amp_Cz  = mean(Cz)) %>%
+  arrange(subject, epoch)
 
-#subject as factor
-eeg_data$subject <- as.factor(eeg_data$subject)
 
-#arrange both frames in ascending order
-eeg_data <- arrange(eeg_data, subject)
-behav_data <- arrange(behav_data, subject)
+# === 3) check number of eppchs and index ======================
+n_eeg_epochs <- xtabs(data = eeg_data, ~ subject)
+n_behav_epochs <- xtabs(data = behav_data, ~ subject)
+n_eeg_epochs == n_behav_epochs
 
-#rename condition variable in eeg data
-eeg_data <- eeg_data %>% rename(halp = "condition")
+# === 4) merge ======================
+# subject as factor
+eeg_data$subject <- as.integer(eeg_data$subject)
+eeg_data$epoch <- as.integer(eeg_data$epoch)
 
-#bind both dataframes
-ern_soc <- cbind(eeg_data, behav_data)
+eeg_data <- data.frame(eeg_data)
+names(eeg_data)[2] <- 'halp'
 
-#remove subject duplicate
-ern_soc <- ern_soc[,-10]
+# bind both dataframes
+ern_soc <- merge(eeg_data, behav_data, c('subject', 'epoch')) 
 
-#halp and reaction as factor  as factor
-ern_soc$halp <- as.factor(ern_soc$halp)
-ern_soc$reaction <-  as.factor(ern_soc$reaction) 
 
-#rename factor levels of reaction
-ern_soc$reaction <- revalue(ern_soc$reaction, c("1" = "richtig", "0" = "falsch") ) 
+# eeg_soc <- eeg_data %>% rename(halp = "condition")
+ern_soc$flanker <- as.factor(ern_soc$flanker)
+ern_soc$halp <-  ern_soc$halp %>% as.factor()
+ern_soc$reaction <- ern_soc$reaction %>% as.factor()
+ern_soc$condition <-  as.factor(ern_soc$condition)
+ern_soc <- ern_soc %>% 
+  dplyr::rename(id = subject)
 
-# filter by wrong reactions
-wrong_data <- filter(ern_soc, reaction == "falsch") 
+table <- ern_soc %>% 
+  group_by(halp, condition) %>% 
+  dplyr::summarise(mean_rt = mean(rt),
+                   mean_FcZ = mean(mean_amp_FCz))
 
-#boxplot for mean amp of FCz by condition
-ggplot(falsch_data, aes(condition, mean_amp_FCz))+
-  geom_boxplot()
 
+
+
+# wrong_answ$condition <- as.factor(wrong_answ$condition)
+# ggplot(wrong_answ, aes(x = condition, y = mean_amp_FCz))+
+#   geom_violin()
+# 
+# mod <- lm(mean_amp_FCz ~ condition, data = wrong_answ)
+# anova(mod)
+# mod2 <- aov(mean_amp_FCz ~ condition, data = wrong_answ)
+# TukeyHSD(mod2)
